28875fb706ace4fe9f9b1e1926327dc6c1064f10
Jad <jad-abou-hawili@hotmail.com>
2025-03-26 11:49:51 +0200

    custom tactic working, resolve the or axiom eith

--- Game/Levels/KnightsAndKnaves/asdf.lean
+++ Game/Levels/KnightsAndKnaves/asdf.lean
@@ -16,19 +16,33 @@ Introduction
 --
 --  }
 
+
 open settheory_approach
 #check dis
 #check Inhabitant
+axiom eith (A : Inhabitant): A ∈ Knight or A ∈ Knave 
+macro "set_knight_to_knave_notiff" t2:term "at"  t1:Lean.Parser.Tactic.locationWildcard : tactic =>
+do`(tactic| simp [inleft_notinrightIff (eith $t2) dis] at $t1)
+
 variable [DecidableEq Inhabitant]
-Statement 
+#check inleft_notinright dis
+#check inleft_notinrightIff
+Statement
 {A : Inhabitant}
+{B : Inhabitant}
+{either : A ∈ Knight ∨ A ∈ Knave}
 (hA : A ∈ Knight )
 (hA' : A ∈ Knave )
-: 1=2 := by 
+(hB : B ∈ Knight)
+(hB' : B ∈ Knave)
+: 1=2 := by
+  -- i can have this take A instead of either as argument then use an axiom that gives the or, this tactic wouldn't work for all instantces of knight
+  #check eith
+  set_knight_to_knave_notiff B at *
   have : False := disjoint_without hA hA'
   contradiction
 
-Conclusion 
+Conclusion
 "
 "
 --NewTheorem settheory_approach.disjoint_without
