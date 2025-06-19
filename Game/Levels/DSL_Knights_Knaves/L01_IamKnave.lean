21f5563c6233935a6d38d954f771e4fbae4bd3e4
Ada Alakbarova <ada.alakbarova@proton.me>
2025-06-19 18:29:35 +0200

    thats/that's

--- Game/Levels/DSL_Knights_Knaves/L01_IamKnave.lean
+++ Game/Levels/DSL_Knights_Knaves/L01_IamKnave.lean
@@ -24,7 +24,7 @@ isKnight_or_isKnave
 : A.isKnight or A.isKnave
 ```
 
-If `A` were a knight, then `A`'s statement is true then `A` is a knave but thats impossible. So, if `A` were a knight then we can prove `False`.
+If `A` were a knight, then `A`'s statement is true then `A` is a knave but that's impossible. So, if `A` were a knight then we can prove `False`.
 
 If `A` were a knave, then `A`'s statement would be false, i.e. `A` is not a knave but thats impossible as well. So, if `A` were a knave then we can prove `False`.
 
